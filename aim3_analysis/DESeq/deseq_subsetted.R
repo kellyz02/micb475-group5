1b9d59aca7321354c7072d63da57d994b722a0bc
ssyl29 <99515359+ssyl29@users.noreply.github.com>
2025-03-26 10:48:17 -0700

    Updated deseq script
    includes certartiodactyla and perissodactyla now :)

--- aim3_analysis/DESeq/deseq_subsetted.R
+++ aim3_analysis/DESeq/deseq_subsetted.R
@@ -23,6 +23,11 @@ primate_phylo <- subset_samples(phylo_plus1,
                                 Taxonomy_Order == "Primates") %>%
   subset_samples(Climate..basic. != "B") %>%
   subset_samples(Climate..basic. != "D")
+certartiodactyla_phylo <- subset_samples(phylo_plus1,
+                                         Taxonomy_Order == "Cetartiodactyla")
+perissodactyla_phylo <- subset_samples(phylo_plus1,
+                                       Taxonomy_Order == "Perissodactyla") %>%
+  subset_samples(Climate..basic. != "D")
 
 
 ### DESeq objects and analysis ###
@@ -37,6 +42,12 @@ pilo <- phyloseq_to_deseq2(pilosa_phylo, ~`Climate..basic.`)
 prim_clim <- phyloseq_to_deseq2(primate_phylo, ~`Climate..basic.`)
 prim_capt <- phyloseq_to_deseq2(primate_phylo, ~`captive_wild`)
 
+# arid and temperate
+cert <- phyloseq_to_deseq2(certartiodactyla_phylo, ~`Climate..basic.`)
+
+# arid and temperate
+peri <- phyloseq_to_deseq2(perissodactyla_phylo, ~`Climate..basic.`)
+
 
 ###
 
@@ -46,6 +57,10 @@ deseq_pilo <- DESeq(pilo)
 
 deseq_prim_clim <- DESeq(prim_clim)
 deseq_prim_capt <- DESeq(prim_capt)
+
+deseq_cert <- DESeq(cert)
+
+deseq_peri <- DESeq(peri)
 
 # Results tables
 # note to self: reference set as earliest in alphabetical order if undefined
@@ -59,6 +74,11 @@ res_prim_clim <- results(deseq_prim_clim, tidy = TRUE)
 res_prim_capt <- results(deseq_prim_capt, tidy = TRUE)
 # reference climate: A (tropical)
 
+res_cert <- results(deseq_cert, tidy = TRUE)
+# reference climate: B (arid)
+
+res_peri <- results(deseq_peri, tidy = TRUE)
+# reference climate: B (arid)
 
 ### Volcano plots ###
 # tubulidentata
@@ -73,19 +93,33 @@ vol_pilo <- res_pilo %>%
   mutate(significant = padj<0.01 & abs(log2FoldChange)>2) %>%
   ggplot() +
   geom_point(aes(x=log2FoldChange, y=-log10(padj), col=significant)) +
-  labs(title = "Differential Expression of Pilosa (Climate)")
+  labs(title = "Differential Expression of Pilosa (Temperate and Continental)")
 
 # primate
 vol_prim_clim <- res_prim_clim %>%
   mutate(significant = padj<0.01 & abs(log2FoldChange)>2) %>%
   ggplot() +
   geom_point(aes(x=log2FoldChange, y=-log10(padj), col=significant)) +
-  labs(title = "Differential Expression of Primates (Climate)")
+  labs(title = "Differential Expression of Primates (Tropical and Temperate)")
 vol_prim_capt <- res_prim_capt %>%
   mutate(significant = padj<0.01 & abs(log2FoldChange)>2) %>%
   ggplot() +
   geom_point(aes(x=log2FoldChange, y=-log10(padj), col=significant)) +
   labs(title = "Differential Expression of Primates (Captive vs. Wild)")
+
+# certartiodactyla
+vol_cert <- res_cert %>%
+  mutate(significant = padj<0.01 & abs(log2FoldChange)>2) %>%
+  ggplot() +
+  geom_point(aes(x=log2FoldChange, y=-log10(padj), col=significant)) +
+  labs(title = "Differential Expression of Certartiodactyla (Arid and Temperate)")
+
+# perissodactyla
+vol_peri <- res_peri %>%
+  mutate(significant = padj<0.01 & abs(log2FoldChange)>2) %>%
+  ggplot() +
+  geom_point(aes(x=log2FoldChange, y=-log10(padj), col=significant)) +
+  labs(title = "Differential Expression of Perissodactyla (Arid and Temperate)")
 
 ### File Saves ###
 ggsave(filename = "tubulidentata_captivity_deseq.png", vol_tubu)
@@ -94,6 +128,10 @@ ggsave(filename = "pilosa_climate_deseq.png", vol_pilo)
 
 ggsave(filename = "primate_climate_deseq.png", vol_prim_clim)
 ggsave(filename = "primate_captivity_deseq.png", vol_prim_capt)
+
+ggsave(filename = "certartiodactyla_climate_deseq.png", vol_cert)
+
+ggsave(filename = "perissodactyla_climate_deseq.png", vol_peri)
 
 
 ### Pulling Significant ASVs ###
@@ -112,4 +150,12 @@ primcapt_sigASVs <- res_prim_capt %>%
   filter(padj<0.01 & abs(log2FoldChange)>2) %>%
   dplyr::rename(ASV=row)
 # captivity had more ASVs of significance
+
+cert_sigASVs <- res_cert %>%
+  filter(padj<0.01 & abs(log2FoldChange)>2) %>%
+  dplyr::rename(ASV=row)
+
+peri_sigASVs <- res_peri %>%
+  filter(padj<0.01 & abs(log2FoldChange)>2) %>%
+  dplyr::rename(ASV=row)
 
