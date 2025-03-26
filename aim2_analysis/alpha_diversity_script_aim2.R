47aef9719807b8f9b964760b263474bd9b0093b2
kellyz02 <kellyz02@student.ubc.ca>
2025-03-26 10:16:00 -0700

    add alpha diversity aim 2

--- aim2_analysis/alpha_diversity_script_aim2.R
+++ aim2_analysis/alpha_diversity_script_aim2.R
@@ -3,15 +3,17 @@
 if (!requireNamespace("picante", quietly = TRUE)) install.packages("picante")
 if (!requireNamespace("ggsignif", quietly = TRUE)) install.packages("ggsignif")
 if (!requireNamespace("FSA", quietly = TRUE)) install.packages("FSA")
+if (!requireNamespace("rstatix", quietly = TRUE)) install.packages("rstatix")
 
 library(FSA)  # for Dunn's test
 library(phyloseq)
 library(tidyverse)
 library(picante)
 library(ggsignif)
+library(rstatix)  
 
 ## To run, please replace with the order name as specified in the metadata.
-order_name <- "Primates"
+order_name <- "Perissodactyla"
 
 # 0. Import phyloseq_file object and save as "phyloseq")
 setwd("~/Documents/UBC/MICB475/project_2/micb475-group5/aim2_analysis")
@@ -33,7 +35,11 @@ shannon_plot <- ggplot(sample_data(phyloseq_order), aes(x = Climate..basic., y =
   xlab("Climate Category") +
   ylab("Shannon Diversity") +
   scale_fill_manual(values = c("wild" = "#1b9e77", "captive" = "#d95f02")) + # Custom colors
-  theme(axis.text.x = element_text(angle = 0, hjust=0.5))
+  theme(axis.text.x = element_text(angle = 0, hjust=0.5)) + 
+  ylim(min(sample_data(phyloseq_order)$Shannon) - 0.05, max(sample_data(phyloseq_order)$Shannon) * 1.1)
+
+shannon_plot
+
 
 # 3. FAITH'S PHYLOGENETIC DIVERSITY: calculate diversity taking into account
 # phylogenetic diversity (i.e. does captivity impact evolutionary diversity/structure of community?)
@@ -49,124 +55,97 @@ pd_plot <- ggplot(sample_data(phyloseq_order), aes(x = Climate..basic., y = PD, 
   ggtitle(paste("Faith's Phylogenetic Diversity of", order_name, "Gut Microbiome Samples Across Climates")) +
   xlab("Climate Category") +
   ylab("Phylogenetic Diversity") +
-  scale_fill_manual(values = c("wild" = "#1b9e77", "captive" = "#d95f02"))
+  scale_fill_manual(values = c("wild" = "#1b9e77", "captive" = "#d95f02")) + 
+  ylim(min(sample_data(phyloseq_order)$PD) - 0.05, max(sample_data(phyloseq_order)$PD) * 1.1)
 
 shannon_plot
 pd_plot
 
 #=======================================================================
-#### Statistical Analysis: Wilcoxon Rank Test (non-parametric, significance of diversity differences) ######
+# # 4. Calculate Kruskal-Wallis of wild vs captivity status across the climates. 
 samp_dat <- sample_data(phyloseq_order)
 samp_dat_df <- data.frame(samp_dat)
 
-num_climates <- length(unique(samp_dat_df$Climate..basic.))
+# kruskal_shannon_captivity <- kruskal.test(Shannon ~ captive_wild, data = samp_dat_df)
+# kruskal_fd_captivity <- kruskal.test(PD ~ captive_wild, data = samp_dat_df)
+# 
+# # Extract p-value
+# kruskal_shannon_p_value <- signif(kruskal_shannon_captivity$p.value, digits = 3)
+# kruskal_fd_p_value <- signif(kruskal_fd_captivity$p.value, digits = 3)
+# 
+# # Add Kruskal-Wallis annotation on boxplots
+# shannon_plot <- shannon_plot +
+#   labs(subtitle = paste("Kruskal-Wallis (wild vs captive across climates) p=", format(kruskal_shannon_p_value, digits = 6)), size = 4)
+# 
+# pd_plot <- pd_plot +
+#   labs(subtitle = paste("Kruskal-Wallis (wild vs captive across climates) p=", format(kruskal_fd_p_value, digits = 6)), size = 4)
+# 
+# 
+# # 5. CALCULATE DUNN'S PAIRWISE COMPARISONS
+# # Run Dunn's test
+# dunn_shannon <- dunnTest(Shannon ~ interaction(Climate..basic., captive_wild), data = samp_dat_df, method = "bh")
+# dunn_pd <- dunnTest(PD ~ interaction(Climate..basic., captive_wild), data = samp_dat_df, method = "bh")
+# dunn_shannon_results <- dunn_shannon$res
+# dunn_pd_results <- dunn_pd$res
+# dunn_shannon_results$Test <- "DUNN Shannon"
+# dunn_pd_results$Test <- "DUNN PD"
+# # #=======================================================================
+#### Statistical Analysis: Wilcoxon Rank Test (non-parametric, significance of diversity differences) ######
 
-if (num_climates == 2) {
-  # Wilcox: Shannon Diversity
-  wilcox_shannon <- wilcox.test(Shannon ~ interaction(Climate..basic., captive_wild), data = samp_dat_df, exact = FALSE)
-  # Wilcox: Faith's PD
-  wilcox_pd <- wilcox.test(PD ~ interaction(Climate..basic., captive_wild), data = samp_dat_df, exact = FALSE)
-  
-  p_value_shannon <- wilcox_shannon$p.value
-  p_value_pd <- wilcox_pd$p.value
-  
-  shannon_plot <- shannon_plot + 
-    geom_signif(comparisons = list(c("B", "C")), 
-                annotations = paste("p =", format(wilcox_shannon$p.value, digits = 6)),  
-                y_position = 6.5,
-                tip_length = 0.03)
-  
-  pd_plot <- pd_plot + 
-    geom_signif(comparisons = list(c("B", "C")), 
-                annotations = paste("p =", format(wilcox_pd$p.value, digits = 6)),  
-                y_position = 102,
-                tip_length = 0.03)
-} else {
-  # Get the unique interaction groups used in Dunn's test
-  group_levels <- unique(interaction(samp_dat_df$Climate..basic., samp_dat_df$captive_wild))
-  
-  # Separate wild and captive groups
-  wild_groups <- grep("wild$", group_levels, value = TRUE)
-  captive_groups <- grep("captive$", group_levels, value = TRUE)
-  
-  # Generate pairwise comparisons **only within each group**
-  wild_comparisons <- combn(wild_groups, 2, simplify = FALSE)
-  captive_comparisons <- combn(captive_groups, 2, simplify = FALSE)
-  
-  # Combine the comparisons
-  comparisons <- c(wild_comparisons, captive_comparisons)
-  comparisons
-  
-  # Check the number of comparisons
-  num_comparisons <- length(comparisons)
-  
-  kruskal_shannon <- kruskal.test(Shannon ~ interaction(Climate..basic., captive_wild), data = samp_dat_df)
-  # Kruskal: Faith's PD
-  kruskal_pd <- kruskal.test(PD ~ interaction(Climate..basic., captive_wild), data = samp_dat_df)
+# # Subset the data for the two specific groups
+subset_rows <- samp_dat_df[samp_dat_df$captive_wild == "captive", ]
 
-  p_value_shannon <- kruskal_shannon$p.value
-  p_value_pd <- kruskal_pd$p.value
-  
-  # Ensure the number of p-values corresponds to the number of comparisons
-  pairwise_p_values_shannon <- dunn_shannon$res$P.adj[1:num_comparisons]
-  pairwise_p_values_pd <- dunn_pd$res$P.adj[1:num_comparisons]
-  pairwise_p_values_pd
-  
-  # Get max values for y-axis positioning
-  max_shannon <- max(samp_dat_df$Shannon, na.rm = TRUE)
-  max_pd <- max(samp_dat_df$PD, na.rm = TRUE)
-  
-  # Set staggered y-axis positions
-  y_positions_shannon <- seq(from = max_shannon * 1.1, 
-                             by = max_shannon * 0.1, 
-                             length.out = num_comparisons)
-  
-  y_positions_pd <- seq(from = max_pd * 1.1, 
-                        by = max_pd * 0.1, 
-                        length.out = num_comparisons)
-  
-  # Plot Shannon Diversity with statistical comparisons
-  shannon_plot <- shannon_plot + 
-    geom_signif(comparisons = comparisons, 
-                annotations = sapply(pairwise_p_values_shannon, function(p) paste("p =", format(p, digits = 6))),  
-                y_position = y_positions_shannon,
-                tip_length = 0.03) +
-    labs(subtitle = paste("Kruskal-Wallis p =", format(p_value_shannon, digits = 6)), size = 4)
-  
-  # Plot Faith's PD with statistical comparisons
-  pd_plot <- pd_plot + 
-    geom_signif(comparisons = comparisons, 
-                annotations = sapply(pairwise_p_values_pd, function(p) paste("p =", format(p, digits = 6))),  
-                y_position = y_positions_pd,
-                tip_length = 0.03) +
-    labs(subtitle = paste("Kruskal-Wallis p =", format(p_value_pd, digits = 6)), size = 4)
-}
+# Perform the Wilcoxon test
+wilcox_shannon <- wilcox.test(Shannon ~ Climate..basic., data = subset_rows, exact = FALSE)
+wilcox_shannon_results <- data.frame(
+  Comparison = "C.captive - D. captive", Z = NA, P.unadj = NA, P.adj = wilcox_shannon$p.value, Test="WILCOX Shannon"
+)
 
+wilcox_pd <- wilcox.test(PD ~ Climate..basic., data = subset_rows, exact = FALSE)
+wilcox_pd_results <- data.frame(
+  Comparison = "C.captive - D. captive", Z = NA, P.unadj = NA, P.adj = wilcox_pd$p.value, Test="WILCOX PD"
+)
+
+# combined_significances <- rbind(dunn_shannon_results, dunn_pd_results, wilcox_shannon_results, wilcox_pd_results)
+combined_significances <- rbind(wilcox_shannon_results, wilcox_pd_results)
+combined_significances$P.adj <- format(combined_significances$P.adj, scientific = FALSE)
+significances_file_name <- paste0("alpha_diversity/", order_name, "_significances.csv")
+write.csv(combined_significances, file = significances_file_name, row.names = FALSE)
+combined_significances
+
+############ FOR PILOSA
+# #### Statistical Analysis: Wilcoxon Rank Test (non-parametric, significance of diversity differences) ######
+# # Perform the Wilcoxon test
+# samp_dat <- sample_data(phyloseq_order)
+# samp_dat_df <- data.frame(samp_dat)
+# 
+# wilcox_shannon <- wilcox.test(Shannon ~ Climate..basic., data = samp_dat_df, exact = FALSE)
+# wilcox_shannon_results <- data.frame(
+#   Comparison = "C.captive - D.captive", Z = NA, P.unadj = NA, P.adj = wilcox_shannon$p.value, Test="WILCOX Shannon"
+# )
+# 
+# wilcox_pd <- wilcox.test(PD ~ Climate..basic., data = samp_dat_df, exact = FALSE)
+# wilcox_pd_results <- data.frame(
+#   Comparison = "C.captive - D.captive", Z = NA, P.unadj = NA, P.adj = wilcox_pd$p.value, Test="WILCOX PD"
+# )
+# 
+# combined_significances <- rbind(wilcox_shannon_results, wilcox_pd_results)
+# combined_significances$P.adj <- format(combined_significances$P.adj, scientific = FALSE)
+# significances_file_name <- paste0("alpha_diversity/", order_name, "_significances.csv")
+# write.csv(combined_significances, file = significances_file_name, row.names = FALSE)
 #=======================================================================
 #### Add Statistical Results to Boxplots ######
 
 shannon_plot
 pd_plot
 
-
 #### Output plots ######
-ggsave(paste0("alpha_shannon_", order_name, "_boxplot_climates.png"),
+ggsave(paste0("alpha_diversity/alpha_shannon_", order_name, "_boxplot_climates.png"),
        shannon_plot,
        height=4, width=6.5)
 
-ggsave(paste0("alpha_pd_", order_name, "_boxplot_climates.png"),
+ggsave(paste0("alpha_diversity/alpha_pd_", order_name, "_boxplot_climates.png"),
        pd_plot,
        height=4, width=6.5)
 
-
-print(comparisons)
-print(unique(samp_dat_df$Climate..basic.))  # Check x-axis factor levels
-print(unique(interaction(samp_dat_df$Climate..basic., samp_dat_df$captive_wild)))  # Check if interactions match
-pairwise_p_values_pd
-pairwise_p_values_shannon
-
-hello <- geom_signif(comparisons = comparisons, 
-            annotations = sapply(pairwise_p_values_pd, function(p) paste("p =", format(p, digits = 6))),  
-            y_position = y_positions_pd,
-            tip_length = 0.03)
 
